7b34a836e22fb715ff22fb0f224a885cc0a6a302
yooshmaa <yooshioo.m@gmail.com>
2022-01-16 06:24:46 +0900

    [other] change radio button to checkbox

--- git_commit_study.php
+++ git_commit_study.php
@@ -1,8 +1,8 @@
 <?php
 $industries = array('financial', 'it', 'media', 'automotive', 'machinery', 'energy', 'retail', 'building', 'other');
 $value = '';
-if (isset($_POST['industry'])) {
-	$value = $_POST['industry'];
+if (isset($_POST['industries'])) {
+	$values = $_POST['industries'];
 }
 ?>
 
@@ -13,23 +13,29 @@ if (isset($_POST['industry'])) {
 <title>Industry Survey</title>
 </head>
 <body>
-<?php if (!isset($_POST['industry'])): ?>
+<?php if (!isset($_POST['industries'])): ?>
 <h1>Industry Survey</h1>
 <form action="git_commit_study.php" method="POST">
-<input type="radio" name="industry" value="FINANCIAL" checked>FINANCIAL
-<input type="radio" name="industry" value="IT">IT
-<input type="radio" name="industry" value="MEDIA">MEDIA
-<input type="radio" name="industry" value="AUTOMOTIVE">AUTMOTIVE
-<input type="radio" name="industry" value="MACHINERY">MACHINERY
-<input type="radio" name="industry" value="ENERGY">ENERGY
-<input type="radio" name="industry" value="RETAIL">RETAIL
-<input type="radio" name="industry" value="BUILDING">BUILDING
-<input type="radio" name="industry" value="OTHER">OTHER
+<input type="checkbox" name="industries[]" value="FINANCIAL">FINANCIAL
+<input type="checkbox" name="industries[]" value="IT">IT
+<input type="checkbox" name="industries[]" value="MEDIA">MEDIA
+<input type="checkbox" name="industries[]" value="AUTOMOTIVE">AUTOMOTIVE
+<input type="checkbox" name="industries[]" value="MACHINERY">MACHINERY
+<input type="checkbox" name="industries[]" value="ENERGY">ENERGY
+<input type="checkbox" name="industries[]" value="RETAIL">RETAIL
+<input type="checkbox" name="industries[]" value="BUILDING">BUILDING
+<input type="checkbox" name="industries[]" value="OTHER">OTHER
 <br>
 <input type="submit" value="submit">
 </form>
 <?php else: ?>
-<?php echo 'You are interested in ' . $value . ' industry.'; ?>
+<?php echo 'You are intereseted in '; ?>
+<br>
+<?php foreach ($values as $value): ?>
+<?php echo $value; ?>
+<br>
+<?php endforeach; ?>
+<?php echo ' industries. '; ?>
 <br>
 <?php echo 'Thank you for your cooperation with this survey.';?>
 <?php endif; ?>
