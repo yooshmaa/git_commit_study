7a81c7c8158dafe590e7e0c24e69105578b856f8
yooshmaa <yooshioo.m@gmail.com>
2022-01-16 05:27:41 +0900

    first commit

---
+++ git_commit_study.php
@@ -0,0 +1,36 @@
+<?php
+$industries = array('financial', 'it', 'media', 'automotive', 'machinery', 'energy', 'retail', 'other');
+$value = '';
+if (isset($_POST['industry'])) {
+	$value = $_POST['industry'];
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Industry Survey</title>
+</head>
+<body>
+<?php if (!isset($_POST['industry'])): ?>
+<h1>Industry Survey</h1>
+<form action="git_commit_study.php" method="POST">
+<input type="radio" name="industry" value="FINANCIAL" checked>FINANCIAL
+<input type="radio" name="industry" value="IT">IT
+<input type="radio" name="industry" value="MEDIA">MEDIA
+<input type="radio" name="industry" value="AUTOMOTIVE">AUTMOTIVE
+<input type="radio" name="industry" value="MACHINERY">MACHINERY
+<input type="radio" name="industry" value="ENERGY">ENERGY
+<input type="radio" name="industry" value="RETAIL">RETAIL
+<input type="radio" name="industry" value="OTHER">OTHER
+<br>
+<input type="submit" value="submit">
+</form>
+<?php else: ?>
+<?php echo 'You are interested in ' . $value . ' industry.'; ?>
+<br>
+<?php echo 'Thank you for your cooperation with this survey.';?>
+<?php endif; ?>
+</body>
+</html>
